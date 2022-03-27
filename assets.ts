bf3b41e6c82a28b182b971f47c5f7103aef547a6
jeenie <devjeenie@gmail.com>
2022-03-27 19:38:52 +0900

    add assets

---
+++ assets.ts
@@ -0,0 +1,7 @@
+export default {
+  logo_r: require('./src/assets/cartier_logo_r.png'),
+  banner_hall: require('./src/assets/cartier_hall.jpg'),
+  banner_pic: require('./src/assets/banner_pic.jpg'),
+  banner_odyssee: require('./src/assets/cartier_odyssee.jpg'),
+  banner_2: require('./src/assets/cartier_banner_2.jpg'),
+};
